6627458a70aecf69624a298863af8e67b88cecd5
Lein-adq <carlos_da2712@hotmail.com>
2023-10-15 21:34:49 -0400

    Initial commit

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/button";
+
+const LoginCard = () => {
+  return ( 
+    <div>
+      <Button>
+        Holi
+      </Button>
+    </div>
+   );
+}
+ 
+export default LoginCard;
